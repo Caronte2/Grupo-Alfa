b349f5137c9a4e16483acfca71b3b33c7395ae88
David B.L <davidbayonalujan@gmail.com>
2024-10-20 19:37:27 +0200

    Iconos

--- app/src/main/java/com/example/zapstation/MainActivity.java
+++ app/src/main/java/com/example/zapstation/MainActivity.java
@@ -29,37 +29,56 @@ import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
     // Nombres de las pestañas
-    private String[] nombres = new String[]{"Bienvenido","","", ""};
+    private String[] nombres = new String[]{"", "", "", "", ""};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ViewPager2 viewPager = findViewById(R.id.viewpager);
-        viewPager.setAdapter(
-                new MiPagerAdapter(this));
+        viewPager.setAdapter(new MiPagerAdapter(this));
+
         TabLayout tabs = findViewById(R.id.tabs);
         new TabLayoutMediator(tabs, viewPager,
                 new TabLayoutMediator.TabConfigurationStrategy() {
                     @Override
-                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position){
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        // Dejar el texto vacío
                         tab.setText(nombres[position]);
+
+                        // Establecer un ícono para cada tab
+                        switch (position) {
+                            case 0:
+                                tab.setIcon(R.drawable.star);
+                                break;
+                            case 1:
+                                tab.setIcon(R.drawable.map_pin);
+                                break;
+                            case 2:
+                                tab.setIcon(R.drawable.home);
+                                break;
+                            case 3:
+                                tab.setIcon(R.drawable.battery_charging);
+                                break;
+                            case 4:
+                                tab.setIcon(R.drawable.settings);
+                                break;
+                        }
                     }
-                }
-        ).attach();
-
+                }).attach();
+        // Establecer Home como la pestaña activa al inicio
+        viewPager.setCurrentItem(2, false); // 2 es el índice de la pestaña Home
     }
-
 
     public static class MiPagerAdapter extends FragmentStateAdapter {
         public MiPagerAdapter(FragmentActivity activity) {
             super(activity);
         }
 
-        //Cambiar el return a n de vistas tengamos
         @Override
         public int getItemCount() {
-            return 1;
+            return 5; // Cambia a la cantidad de pestañas que tienes
         }
 
         @Override
@@ -68,12 +87,14 @@ public class MainActivity extends AppCompatActivity {
             switch (position) {
                 case 0:
                     return new Tab1();
-                /*case 1:
+                case 1:
                     return new Tab2();
                 case 2:
                     return new Tab3();
                 case 3:
-                    return new Tab4();*/
+                    return new Tab4();
+                case 4:
+                    return new Tab5();
             }
             return null;
         }
